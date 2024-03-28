40376d6c7f8b6d8dfa334b43aed2b47bea12937a
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-03-28 13:16:33 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,12 +3,6 @@ using namespace std;
 //probalby hold the values in the form:
 //umap<String, vector<String>>
 
-void sortAllWords(){ // sort the word list by number of unique letters
-  cout << '';
-}
-
-
-
 int main(){
-    sortAllWords();
+    return 0;
 }

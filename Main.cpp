60fb2c31c99cd35d2a1708f807c63f4db2e6a670
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-04-04 10:44:36 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -8,39 +8,41 @@
 using namespace std;
 //probalby hold the values in the form:
 //umap<String, vector<String>>
-vector<string> words;
+vector<pair<string,int>> words_and_scores;
 
-bool _sort (string word1, string word2){
-    unordered_set<char> a (word1.begin(), word1.end());
-    unordered_set<char> b (word2.begin(), word2.end());
-    return a.size() > b.size();
+bool _sort (pair<string,int> w1, pair<string,int> w2){
+    return w1.second > w2.second;
 }
 
 void sort_words_by_unique(){
-    string word="";
     ifstream read("Wordlist.txt");
-    int i=0;
+    string word="";
     while (getline (read, word)) {
-        if(word.size() > 1)words.emplace_back(word);
-        ++i;
+        if(word.size() > 1){
+            pair<string,int> t;
+            t.first = word;
+            unordered_set<char> s (word.begin(),word.end());
+            t.second = s.size();
+            words_and_scores.emplace_back(t);
+        }
     }
     read.close();
-    sort(execution::par, words.begin(), words.end(),_sort);
+    sort(execution::par, words_and_scores.begin(), words_and_scores.end(),_sort);
 }
 
-void save_sorted_words(){
-    ofstream output_sorted("Sortedwords.txt", ios::out);
-    for(const auto& word : words){
-        output_sorted << word << '\n';
-    }
-    output_sorted.close();
-}
+// void save_sorted_words(){
+//     ofstream output_sorted("Sortedwords.txt", ios::out);
+//     for(const auto& word : words){
+//         output_sorted << word << '\n';
+//     }
+//     output_sorted.close();
+// }
 
 int main(){
     sort_words_by_unique();
-    for(const auto& x : words){
-        cout<<x<<'\n';
+    for(const auto& x : words_and_scores){
+        cout << x.first << " - " << x.second << '\n';
     }
-    save_sorted_words();
+    //save_sorted_words();
     return 0;
 }

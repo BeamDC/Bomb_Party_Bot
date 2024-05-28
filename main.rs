7aaea0119629a354a76682e748a8b118d4aae7d1
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-05-28 17:08:06 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,65 +1,53 @@
-use std::fs::read_to_string;
+use std::fs::File;
 use std::cmp::Reverse;
 use std::path::Path;
 use std::collections::{HashSet, HashMap};
+use std::io;
+use std::io::BufRead;
+use lazy_static::lazy_static;
 
-fn file_to_vec(path: &str) -> Vec<String>{
+lazy_static! {
+        static ref SCORES: HashMap<char, u8> = {
+            let mut m = HashMap::new();
+            m.insert('A', 1); m.insert('E', 1); m.insert('I', 1); m.insert('O', 1); m.insert('U', 1);
+            m.insert('R', 1); m.insert('S', 1); m.insert('T', 1); m.insert('L', 1); m.insert('N', 1);
+            m.insert('D', 2); m.insert('G', 2); m.insert('B', 3); m.insert('C', 3); m.insert('M', 3);
+            m.insert('P', 3); m.insert('F', 4); m.insert('H', 4); m.insert('V', 4); m.insert('W', 4);
+            m.insert('Y', 4); m.insert('K', 5); m.insert('J', 8); m.insert('Q', 10); m.insert('X', 0);
+            m.insert('Z', 0);
+            m
+        };
+    }
+
+fn file_to_vec(path: &str) -> io::Result<Vec<String>> {
     let path = Path::new(path);
-
-    read_to_string(path) 
-        .unwrap()
-        .lines()
-        .map(String::from)
-        .collect()
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+    Ok(reader.lines().map(|line| line.unwrap()).collect())
 }
 
 // fn to score a word
-fn score(word: &str) -> u8{
-    // remove duplicate chars
-    let mut seen = HashSet::new();
-    let word:String = word.chars().filter(|c| seen.insert(*c)).collect();
-    // print!("{} <- ",word);
-
-    // calculate score
-    let mut scores = HashMap::new();
-    scores.insert('A', 1);
-    scores.insert('E', 1);
-    scores.insert('I', 1);
-    scores.insert('O', 1);
-    scores.insert('U', 1);
-    scores.insert('R', 1);
-    scores.insert('S', 1);
-    scores.insert('T', 1);
-    scores.insert('L', 1);
-    scores.insert('N', 1);
-    scores.insert('D', 2);
-    scores.insert('G', 2);
-    scores.insert('B', 3);
-    scores.insert('C', 3);
-    scores.insert('M', 3);
-    scores.insert('P', 3);
-    scores.insert('F', 4);
-    scores.insert('H', 4);
-    scores.insert('V', 4);
-    scores.insert('W', 4);
-    scores.insert('Y', 4);
-    scores.insert('K', 5);
-    scores.insert('J', 8);
-    scores.insert('Q', 10);
-    scores.insert('X', 0);
-    scores.insert('Z', 0);
-    
-    let mut val:u8 = 0;
-    for c in word.chars(){
-        val += scores.get(&c).unwrap();
+fn score(word: &str) -> u8 {
+    let mut score = 0;
+    for c in word.chars() {
+        match SCORES.get(&c) {
+            Some(s) => score += *s,
+            None => (),
+        }
     }
-    val
+    score
 }
 
-fn main(){
-    let mut vec:Vec<String> = file_to_vec("Input.txt");
+fn main() {
+    use std::time::Instant;
+    let now = Instant::now();
+
+    let mut vec: Vec<String> = match file_to_vec("F:\\Programming\\Ethan\\Rust\\Bomb_Party_Solver\\src\\Wordlist.txt"){
+        Ok(words) => words,
+        Err(e) => panic!("Error reading file: {}", e),
+    };
     vec.sort_by_key(|s| Reverse(score(&s))); // sort values by score
-    for s in vec{
-        println!("{} -> {}",s, score(&s));
-    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 }

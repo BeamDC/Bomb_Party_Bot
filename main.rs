ba72f4e3020d396d077a0e0464a0b6afc2ab75c9
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-06-06 22:39:39 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,35 +1,25 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, Write};
 use std::path::Path;
 
-use aho_corasick::{AhoCorasick, Match};
+use aho_corasick::AhoCorasick;
 use eframe::egui;
 use egui::Color32;
-use lazy_static::lazy_static;
 
 /// scoring, sorting, saving words
-// when words are played, set the values of those letters to zero, 
-// find all words that match a prompt, then sort those words by the new scores 
+// when words are played, set the values of those letters to zero,
+// find all words that match a prompt, then sort those words by the new scores
 // instead of hash map, use a vec in main (self.score
-// pass this vector ovewr to the scoring function
+// pass this vector over to the scoring function
 // this also makes changing the score easier, if lazystatics cant be modified easily
 // values can be modified using let Some(c) = SCORES.get_mut(&charval); *c = newval
 // https://play.rust-lang.org/?version=stable&mode=debug&edition=2021&gist=94631989e4d6ceb93a6988153c7d72d4
-// probably rewite this as a normal hash map, if above method does not work
+// probably rewrite this as a normal hash map, if above method does not work
 // also maybe make a module with all the non gui functionality, for organisation
-lazy_static! {
-        static ref SCORES: HashMap<char, u8> = {
-            let mut m = HashMap::new();
-            m.insert('A', 1); m.insert('B', 1); m.insert('C', 1); m.insert('D', 1); m.insert('E', 1);
-            m.insert('F', 1); m.insert('G', 1); m.insert('H', 1); m.insert('I', 1); m.insert('J', 1);
-            m.insert('K', 1); m.insert('L', 1); m.insert('M', 1); m.insert('N', 1); m.insert('O', 1);
-            m.insert('P', 1); m.insert('Q', 1); m.insert('R', 1); m.insert('S', 1); m.insert('T', 1);
-            m.insert('U', 1); m.insert('V', 1); m.insert('W', 1); m.insert('Y', 1);
-            m
-        };
-    }
+
+static ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWY";
 
 fn file_to_vec(path: &str) -> io::Result<Vec<String>> {
     let path = Path::new(path);
@@ -39,11 +29,11 @@ fn file_to_vec(path: &str) -> io::Result<Vec<String>> {
 }
 
 // get the score of a word
-fn score(word: &str) -> u8 {
+fn score(word: &str, scores: &HashMap<char, u8>) -> u8 {
     let mut score = 0;
-    // remove all diuplicate letters
+    // remove all duplicate letters
     for c in word.chars() {
-        match SCORES.get(&c) {
+        match scores.get(&c) {
             Some(s) => score += *s,
             None => (),
         }
@@ -51,39 +41,44 @@ fn score(word: &str) -> u8 {
     score
 }
 
-// sort all words by best score, output to file
-fn sort_and_save() -> io::Result<()>{
-    let mut words: Vec<String> = match file_to_vec("Wordlist.txt"){
+// sort all words by number of unique letters, output to file
+fn sort_and_save() -> io::Result<()> {
+    let mut words: Vec<String> = match file_to_vec("Wordlist.txt") {
         Ok(words) => words,
         Err(e) => panic!("Error reading file: {}", e),
     };
-    words.sort_unstable_by(|a, b| score(b).cmp(&score(a))); // sort values by score
+    words.sort_unstable_by(|a, b| {
+        let a_unique: HashSet<char> = a.chars().filter(|c| c.is_alphabetic()).collect();
+        let b_unique: HashSet<char> = b.chars().filter(|c| c.is_alphabetic()).collect();
+
+        b_unique.len().cmp(&a_unique.len())
+    });
     let mut file = File::options().write(true).open("Sorted_Words.txt")?;
 
-    for word in words{
+    for word in words {
         writeln!(file, "{}", word)?;
     }
     Ok(())
 }
 
 /// loading words, searching by prompt, output handling
-fn load_words() -> Vec<String>{
-    let words:Vec<String> = match file_to_vec("Sorted_Words.txt"){
+fn load_words() -> Vec<String> {
+    let words: Vec<String> = match file_to_vec("Sorted_Words.txt") {
         Ok(words) => words,
         Err(e) => panic!("Error reading file: {}", e),
     };
     words
 }
 
-fn search_by_prompt(words: &String, prompt: &str) -> (String,usize,usize){
+fn search_by_prompt(words: &String, prompt: &str) -> (String, usize, usize) {
     let pattern = [prompt];
 
     // find first substring matching the prompt
     let ac = AhoCorasick::new(pattern).unwrap();
     let mat = ac.find(&words);
 
-    let Some(matches) = Some(mat) else { return ("NO MATCH".to_string(),0,0); } ;
-    if matches.is_none() {return ("NO MATCH".to_string(),0,0);}
+    let Some(matches) = Some(mat) else { return ("NO MATCH".to_string(), 0, 0); };
+    if matches.is_none() { return ("NO MATCH".to_string(), 0, 0); }
 
     // expand the substring to contain the full word
     let mut start = mat.expect("still no match?").start();
@@ -98,13 +93,14 @@ fn search_by_prompt(words: &String, prompt: &str) -> (String,usize,usize){
         end += 1;
     }
 
-    (words[start..end].to_string(),start,end)
+    (words[start..end].to_string(), start, end)
 }
 
 /// GUI
 struct MainWindow {
     prompt: String,
     words: String,
+    scores: HashMap<char,u8>,
     best_word: String,
     start: usize,
     end: usize,
@@ -112,9 +108,24 @@ struct MainWindow {
 
 impl Default for MainWindow {
     fn default() -> Self {
+        let mut scores = HashMap::with_capacity(24);
+        scores.insert('A', 1); scores.insert('B', 1);
+        scores.insert('C', 1); scores.insert('D', 1);
+        scores.insert('E', 1); scores.insert('F', 1);
+        scores.insert('G', 1); scores.insert('H', 1);
+        scores.insert('I', 1); scores.insert('J', 1);
+        scores.insert('K', 1); scores.insert('L', 1);
+        scores.insert('M', 1); scores.insert('N', 1);
+        scores.insert('O', 1); scores.insert('P', 1);
+        scores.insert('Q', 1); scores.insert('R', 1);
+        scores.insert('S', 1); scores.insert('T', 1);
+        scores.insert('U', 1); scores.insert('V', 1);
+        scores.insert('W', 1); scores.insert('Y', 1);
+
         Self {
             prompt: Default::default(),
             words: load_words().join(" "),
+            scores,
             best_word: Default::default(),
             start: 0,
             end: 0,
@@ -138,25 +149,39 @@ impl eframe::App for MainWindow {
                 // play the word
                 // replace the word with a string that will never match, so that no words are reused
                 self.words.replace_range(self.start..self.end, "-");
+                // change scoring for words to align with unused letters
+                for c in self.best_word.chars(){
+                    if let Some(x) = self.scores.get_mut(&c){
+                        *x = 0;
+                    }
+                }
+                //reset the scores if all letters have been used
+                if score(ALPHABET, &self.scores) == 0{
+                    for c in ALPHABET.chars() {
+                        if let Some(x) = self.scores.get_mut(&c){
+                            *x = 1;
+                        }
+                    }
+                }
             }
 
             ui.horizontal(|ui| {
-                if self.best_word == "NO MATCH" { ui.colored_label(Color32::RED, &self.best_word); }
-                else { ui.colored_label(Color32::WHITE, &self.best_word); }
+                if self.best_word == "NO MATCH" { ui.colored_label(Color32::RED, &self.best_word); } else { ui.colored_label(Color32::WHITE, &self.best_word); }
             });
             ui.end_row();
         });
-
     }
 }
 
-fn main() -> Result<(), eframe::Error>{
+fn main() -> Result<(), eframe::Error> {
+
+
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default().with_inner_size([640.0, 480.0]),
         ..Default::default()
     };
-    eframe::run_native("Bomb Party Solver",options,
+    eframe::run_native("Bomb Party Solver", options,
                        Box::new(|cc| {
                            Box::<MainWindow>::default()
                        }),

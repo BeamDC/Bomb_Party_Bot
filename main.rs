564255f345ae93cefe7a88fdcf3a1f5c66cd0863
BIIN <116185629+BeamDC@users.noreply.github.com>
2024-08-08 10:25:59 -0400

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,41 +1,39 @@
 /*
-* TODO:
-* - make macros to read keypresses,
-*   - have one to set the location of the bomb
-*   - have another to got to that location and copy the prompt
-* - when the prompt is copied, pass it to the search fn
-* - when the best word is found use enigo to automatically type it
-*   - this might require a macro to set the input area
-* - repurpose the egui to be a setting menu for the bot
-*   - once the setting are set, close the window and start the bot
-* - check speed using wordlist sorted by score and by alpha, score sorting might be pointless
+* TODO: idk
+*/
+
+/*
+* - input method inspired by: https://github.com/nicolaspiet/jklm-word-bot/tree/main
+*/
+
+/*
+* CURRENTLY UNDERWAY:
+* make sure that the words vec is within main
+* because removing the word from the vector does nothing
+* if it is re-initialized every call
 */
 
 // data related
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
-use std::io;
+use std::{io, thread, time};
 use std::io::{BufRead, Write};
 use std::path::Path;
 use std::sync::{Arc, Mutex};
 use std::time::Instant;
+use rayon::prelude::*;
 
 // keyboard & mouse related (and reading from clipboard)
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
-use crossterm::{
-    event::{Event, KeyCode, poll, read}
-    ,
-    terminal::enable_raw_mode,
-};
-use enigo::{
-    Button, Coordinate,
-    Direction::{Click, Press, Release},
-    Enigo, Key, Keyboard, Mouse, Settings
-};
-use rayon::prelude::*;
+use rdev::{listen, Event, simulate, Button, EventType, Key, SimulateError};
+use rdev::EventType::KeyPress;
+use rdev::Key::{F4};
+use autopilot;
+use lazy_static;
 
 /// scoring, sorting, saving words
 static ALPHABET: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+static TYPING_DELAY: u64 = 1; // 1 is the smallest ths can safely go
 
 fn file_to_vec(path: &str) -> io::Result<Vec<String>> {
     let path = Path::new(path);
@@ -67,7 +65,7 @@ fn score(word: &str, scores: &HashMap<char, u8>) -> i8 {
 
 // sort all words by number of unique letters, output to file
 fn sort_and_save() -> io::Result<()> {
-    let mut words: Vec<String> = match file_to_vec("F:\\Programming\\Ethan\\Rust\\Bomb_Party_Solver\\src\\Wordlist.txt") {
+    let mut words: Vec<String> = match file_to_vec("Wordlist.txt") {
         Ok(words) => words,
         Err(e) => panic!("Error reading file: {}", e),
     };
@@ -76,7 +74,7 @@ fn sort_and_save() -> io::Result<()> {
         let b_unique: HashSet<char> = b.chars().filter(|c| c.is_alphabetic()).collect();
 
         b_unique.len().cmp(&a_unique.len())});
-    let mut file = File::options().write(true).open("F:\\Programming\\Ethan\\Rust\\Bomb_Party_Solver\\src\\Sorted_Words.txt")?;
+    let mut file = File::options().write(true).open("Sorted_Words.txt")?;
 
     for word in words {
         writeln!(file, "{}", word)?;
@@ -86,13 +84,14 @@ fn sort_and_save() -> io::Result<()> {
 
 /// loading words, searching by prompt, output handling
 fn load_words() -> Vec<String> {
-    let words: Vec<String> = match file_to_vec("F:\\Programming\\Ethan\\Rust\\Bomb_Party_Solver\\src\\Sorted_Words.txt") {
+    let words: Vec<String> = match file_to_vec("Sorted_Words.txt") {
         Ok(words) => words,
         Err(e) => panic!("Error reading file: {}", e),
     };
     words
 }
 
+// given a list of possible answers, find the highest scoring word, with the given game state
 fn find_best_word(vec: &Vec<(usize, String)>, scores: &HashMap<char, u8>) -> Option<(usize, String)> {
     let max_score = Arc::new(Mutex::new(i8::MIN));
     let result = Arc::new(Mutex::new(None));
@@ -115,6 +114,7 @@ fn find_best_word(vec: &Vec<(usize, String)>, scores: &HashMap<char, u8>) -> Opt
     Some(<Option<(usize, String)> as Clone>::clone(&result_value).unwrap_or_default())
 }
 
+// given a prompt, search through the wordlist to find all words containing the prompt
 fn search_by_prompt(words: &[String], prompt: &str, scores: &HashMap<char, u8>) -> (usize, String) {
     let start = Instant::now();
     let filter_start = Instant::now();
@@ -127,7 +127,8 @@ fn search_by_prompt(words: &[String], prompt: &str, scores: &HashMap<char, u8>) 
     println!("word filtering completed in: {:.2?}",filter_elapsed);
 
 
-    return if matches.is_empty() { (0, "NO MATCH".to_string()) } else {
+    return if matches.is_empty() { (0, "NO MATCH".to_string()) }
+    else {
         // println!("{:?}",matches);
         let sorting_start = Instant::now();
         let best_word = find_best_word(&mut matches, scores);
@@ -140,12 +141,62 @@ fn search_by_prompt(words: &[String], prompt: &str, scores: &HashMap<char, u8>) 
     }
 }
 
+fn type_string(string: &str){
+    println!("Parsing string... [{}]", string);
+    let string = string;
+    println!("Creating keymap...");
+    let mut keys = HashMap::with_capacity(26);
+    println!("Assigning keys...");
+    keys.insert('A', Key::KeyA); keys.insert('B', Key::KeyB);
+    keys.insert('C', Key::KeyC); keys.insert('D', Key::KeyD);
+    keys.insert('E', Key::KeyE); keys.insert('F', Key::KeyF);
+    keys.insert('G', Key::KeyG); keys.insert('H', Key::KeyH);
+    keys.insert('I', Key::KeyI); keys.insert('J', Key::KeyJ);
+    keys.insert('K', Key::KeyK); keys.insert('L', Key::KeyL);
+    keys.insert('M', Key::KeyM); keys.insert('N', Key::KeyN);
+    keys.insert('O', Key::KeyO); keys.insert('P', Key::KeyP);
+    keys.insert('Q', Key::KeyQ); keys.insert('R', Key::KeyR);
+    keys.insert('S', Key::KeyS); keys.insert('T', Key::KeyT);
+    keys.insert('U', Key::KeyU); keys.insert('V', Key::KeyV);
+    keys.insert('W', Key::KeyW); keys.insert('X', Key::KeyX);
+    keys.insert('Y', Key::KeyY); keys.insert('Z', Key::KeyZ);
 
-fn main() {
-    // setup things
-    let mut enigo = Enigo::new(&Settings::default()).unwrap();
-    let mut scores = HashMap::with_capacity(24);
-    let mut ctx = ClipboardContext::new().unwrap();
+    println!("Typing... [{}]", string);
+    for c in string.chars(){
+        let current_key = keys[&c];
+
+        send(&EventType::KeyPress(current_key));
+        send(&EventType::KeyRelease(current_key));
+        println!("Typed -> [{}]", c);
+    }
+
+    send(&EventType::KeyPress(Key::Return));
+    send(&EventType::KeyRelease(Key::Return));
+}
+
+// send keyboard and mouse events
+fn send(event_type: &EventType) {
+    let delay = time::Duration::from_millis(TYPING_DELAY);
+    match simulate(event_type) {
+        Ok(()) => (),
+        Err(SimulateError) => {
+            println!("event could not be sent:\n{:?}\n", event_type);
+        }
+    }
+    // Let the OS catch up
+    thread::sleep(delay);
+}
+
+fn play(){
+    let mut words_vec: Vec<String> = load_words();
+
+    let mut clipboard_ctx = ClipboardContext::new().unwrap();
+    let mut prompt: String = String::new();
+
+    let mut best_word: String = String::new();
+    let mut word_index: usize = 0usize;
+    let mut scores = HashMap::with_capacity(26);
+
     scores.insert('A', 1); scores.insert('B', 1);
     scores.insert('C', 1); scores.insert('D', 1);
     scores.insert('E', 1); scores.insert('F', 1);
@@ -159,94 +210,87 @@ fn main() {
     scores.insert('U', 1); scores.insert('V', 1);
     scores.insert('W', 1); scores.insert('X', 1);
     scores.insert('Y', 1); scores.insert('Z', 1);
-    let mut saved_pos: (i32, i32) = (0, 0);
-    let mut prompt: String = String::new();
-    let mut words_vec: Vec<String> = Vec::new();
-    let mut best_word: String = String::new();
-    let mut word_index: usize = 0usize;
-    match file_to_vec("F:\\Programming\\Ethan\\Rust\\Bomb_Party_Solver\\src\\Sorted_Words.txt") {
-                Ok(words) => words_vec = words,
-                Err(e) => panic!("Error reading file: {}", e),
-            };
 
-    enable_raw_mode().expect("Failed to enter raw mode");
-    //gets the current mose pos once the button is pressed
 
-    // use f2 and f4 as function keys
-    loop {
-        if poll(std::time::Duration::from_millis(100)).unwrap() {
-            match read().unwrap() {
-                Event::Key(key) => match key.code {
-                    KeyCode::F(2) => {
-                        saved_pos = enigo.location().unwrap();
-                        println!("Mouse Position: {:?}", saved_pos);
-                    }
-                    KeyCode::F(4) => {
-                        println!("F4 key pressed th");
-                        // go to the set position
-                        enigo.move_mouse(saved_pos.0, saved_pos.1, Coordinate::Abs).unwrap();
 
-                        // double click
-                        enigo.button(Button::Left, Click).expect("error pressing left mouse");
-                        enigo.button(Button::Left, Click).expect("error pressing left mouse");
+    // double click left mouse
+    send(&EventType::ButtonPress(Button::Left));
+    send(&EventType::ButtonRelease(Button::Left));
+    send(&EventType::ButtonPress(Button::Left));
+    send(&EventType::ButtonRelease(Button::Left));
 
-                        //copy
-                        enigo.key(Key::Control, Press).expect("error pressing control");
-                        enigo.key(Key::Unicode('c'), Click).expect("error pressing C");
-                        enigo.key(Key::Control, Release).expect("error releasing control");
+    // copy selected text
+    send(&EventType::KeyPress(Key::ControlLeft));
+    send(&EventType::KeyPress(Key::KeyC));
+    send(&EventType::KeyRelease(Key::KeyC));
+    send(&EventType::KeyRelease(Key::ControlLeft));
 
-                        //get the copied prompt and save it here
-                        match ctx.get_contents() {
-                            Ok(contents) => {
-                                prompt = contents.clone();
-                                println!("Clipboard contents: {}", contents);
-                            },
-                            Err(e) => eprintln!("Error getting clipboard contents: {}", e),
-                        }
+    // get the copied text as a string
+    match clipboard_ctx.get_contents() {
+        Ok(contents) => {
+            prompt = contents.clone();
+            println!("Clipboard contents: {}", contents);
+        },
+        Err(e) => eprintln!("Error getting clipboard contents: {}", e),
+    }
 
-                        // find & save the best word
-                        let ans = search_by_prompt(&words_vec, &prompt, &scores);
+    // find & save the best word
+    let prompt: String = prompt.to_uppercase().chars().filter(|c| c.is_alphabetic()).collect();
+    let ans = search_by_prompt(&words_vec, &prompt, &scores);
 
-                        best_word = ans.1;
-                        word_index = ans.0;
+    best_word = ans.1;
+    word_index = ans.0;
 
-                        //ensure best word is valid, if not, don't play
-                        // (we take a knee here and lose a life lol)
-                        if best_word == "NO MATCH" {
-                            println!("NO MATCH FOUND!\n\n");
-                            continue;
-                        }
+    //ensure best word is valid, if not, don't play
+    // (we take a knee here and lose a life lol)
+    if best_word == "NO MATCH" {
+        println!("NO MATCH FOUND!\n\n");
+        // stop function here somehow
+    }
 
-                        //write the word to the output
-                        for c in best_word.chars(){
-                            enigo.key(Key::Unicode(c), Click).expect("error typing word");
-                            // add a delay here in case it needs to feel real
-                        }
-                        enigo.key(Key::Return, Click).expect("error entering word");
+    // write the word to output
+    // (word, flags, wpm, noise)
+    type_string(&best_word);
 
-                        // delete the word
-                        words_vec.remove(word_index);
+    // delete the word
+    words_vec.remove(word_index);
 
-                        // change scoring for words to align with unused letters
-                        for c in best_word.chars(){
-                            if let Some(x) = scores.get_mut(&c){
-                                *x = 0;
-                            }
-                        }
+    // change scoring for words to align with unused letters
+    for c in best_word.chars(){
+        if let Some(x) = scores.get_mut(&c){
+            *x = 0;
+        }
+    }
 
-                        //reset the scores if all letters have been used
-                        if score(ALPHABET, &scores) == 0{
-                            for c in ALPHABET.chars() {
-                                if let Some(x) = scores.get_mut(&c){
-                                    *x = 1;
-                                }
-                            }
-                        }
-                    }
-                    _ => {}
-                },
-                _ => {}
+    // reset the scores if all letters have been used
+    if score(ALPHABET, &scores) == 0{
+        for c in ALPHABET.chars() {
+            if let Some(x) = scores.get_mut(&c){
+                *x = 1;
             }
         }
+    }
+}
+
+// callback for detecting and handling keyboard inputs
+fn callback(event: Event){
+    if event.event_type == KeyPress(F4){
+        play();
+    }
+}
+
+fn main() {
+    // setup things
+    // let mut words_vec: Vec<String> = Vec::new();
+    // match file_to_vec("Sorted_Words.txt") {
+    //     Ok(words) => words_vec = words,
+    //     Err(e) => panic!("Error reading file: {}", e),
+    // };
+
+    // read from callback, notify of any errors
+    if let Err(error) = listen(callback) {
+        println!("Error:\n{:?}\n", error)
+    }else{
+        println!("SUCCESS");
     }
 }
